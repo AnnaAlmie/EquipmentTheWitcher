0e9915cf9f76aef86b5b795ee793872d2bbbac18
AnnaAlmie <annalitvinenko9@gmail.com>
2023-02-11 21:46:45 +0200

    first idea

---
+++ src/components/TheTableArmor.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import { levels, equipments, schools } from '../language/ua';
+
+let titles = computed(() => {
+    let arr = levels.map((level) => {
+        return equipments.map((equipment) => {
+            return level + " " + equipment
+        })
+    })
+    return Array.prototype.concat.apply([], arr)
+})
+
+</script>
+
+<template>
+    <div>
+        <table>
+            <thead>
+                <td>Школа</td>
+                <td v-for="school of schools" v-text="school"></td>
+            </thead>
+            <tbody>
+                <tr v-for="title in titles">
+                    <td>{{ title }}</td>
+                    <td v-for="item in schools.length">1</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<style lang="scss">
+
+</style>
