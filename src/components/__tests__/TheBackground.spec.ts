2f55893357dfa80013afee30e486996d046fddd6
AnnaAlmie <annalitvinenko9@gmail.com>
2023-02-20 17:00:06 +0200

    del

--- src/components/__tests__/TheBackground.spec.ts
+++ src/components/__tests__/TheBackground.spec.ts
@@ -1,10 +1,7 @@
 import { describe, it, expect } from 'vitest'
-import { mount } from '@vue/test-utils'
-import TheBackground from '@/components/TheBackground.vue'
+import { mount } from '@vue/test-utils';
+import TheBackground from '@/components/TheBackground.vue';
 
-describe('TheBackground', () => {
-    //   it('renders properly', () => {
-    //     const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    //     expect(wrapper.text()).toContain('Hello Vitest')
-    //   })
-})
+describe('background', () => {
+
+});
